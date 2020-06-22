9dc74d30c1717ab34c7e0e4e722bfc34a0a29b6d
Abbie Coggins <abbie.coggins@reedbusiness.com>
2020-06-22 17:25:17 +0100

    string calculater class added

---
+++ StringCalculator.java
@@ -0,0 +1,15 @@
+public class StringCalculator{
+    StringCalculator(){
+    }
+
+
+    public String add(String str){
+        String[] strArray = str.split(",");
+
+        int sum = 0;
+        for (String value : strArray) {
+            sum += Integer.parseInt(value);
+        }
+        return Integer.toString(sum);
+    }
+}
